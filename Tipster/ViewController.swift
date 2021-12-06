022c7be4bbc9248dede62c82345e2db2e41710ca
lamia <lamia.fd123@gmail.com>
2021-12-06 13:26:02 +0300

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by لمياء فالح الدوسري on 02/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
